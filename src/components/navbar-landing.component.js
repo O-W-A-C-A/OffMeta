2ca904c8efbcb70e9843ae4016a390b30d667db9
Jesus <jesusojedapvcs@gmail.com>
2020-02-23 18:09:52 -0800

    Landing Page Updates
    Updated landing page to look like prototype, will need to adjust for different resolutions later

--- src/components/navbar-landing.component.js
+++ src/components/navbar-landing.component.js
@@ -4,9 +4,9 @@ export default class NavBar extends Component{
     render(){
         return(
             <div className="App">
-                <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+                <nav className="navbar navbar-expand-lg navbar-land fixed-top">
                     <div className="container">
-                        <Link className="navbar-title" to={"/"} style={{color: '#fff'}}>OffMeta</Link>
+                        <Link className="navbar-title-landing" to={"/"} style={{color: '#fff'}}>OffMeta</Link>
                         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                             <ul className="navbar-nav ml-auto">
                                 <li>

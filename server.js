efbb89632ca6b87174320714fc8acdd09ef0eda8
Irvin <irvinf413@gmail.com>
2020-03-13 09:42:23 -0700

    Test

--- server.js
+++ server.js
@@ -24,3 +24,4 @@ app.use('/api/users', require('./routes/api/users'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server started on port ' + PORT));
+console.log('Hi');

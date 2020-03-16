3ea98af6fa44cfed2097d180161fd65cc0d480c4
Jesus <jesusojedapvcs@gmail.com>
2020-03-15 23:20:11 -0700

    updated league settings and my league ui
    updated ui to reflect something representative of the final product

--- src/components/league-settings.component.js
+++ src/components/league-settings.component.js
@@ -3,7 +3,62 @@ export default class LeagueSettings extends Component{
     render(){
         return(
             <div className="league-settings-panel">
-                <h1>you are in league settings</h1>
+                <div className="settings-section form-section">
+                    <div className="settings-header">Co-owners</div>
+                    <div className="coowner-button">
+                        <button className="manage-owner-btn">Manage Co-owners</button>
+                    </div>
+                </div>
+
+                <div className="settings-section form-section">
+                    <div className="settings-header">League Settings
+                        <div class="right-edit">
+                            <i>Edit League</i>
+                        </div>
+                    </div>
+                    <div className="settings-row-stack">
+                        <div className="settings-name">Number of Teams</div>
+                        <div className="settings-value">10</div>
+                    </div>
+                    <div className="settings-row-stack">
+                        <div className="settings-name">Roster</div>
+                        <div className="settings-value">2 DPS, 2 TNK, 2 SPT, 3 BN</div>
+                    </div>
+                    <div className="settings-row-stack">
+                        <div className="settings-name">Draft Pick Trading Allowed</div>
+                        <div className="settings-value">Yes</div>
+                    </div>
+                </div>
+
+                
+                <div className="settings-section form-section">
+                    <div className="settings-header">Scoring Settings
+                        <div class="right-edit">
+                            <i>Edit Scoring</i>
+                        </div>
+                    </div>
+                    <div className="grouping-header">Eliminations</div>
+                    <div className="settings-row">
+                        <div className="settings-name2">Placeholder</div>
+                        <div className="settings-value2">
+                            <span className="plus">test</span>
+                        </div>
+                    </div>
+                    <div className="grouping-header">Objective Time</div>
+                    <div className="settings-row">
+                        <div className="settings-name2">Placeholder</div>
+                        <div className="settings-value2">
+                            <span className="plus">test</span>
+                        </div>
+                    </div>
+                    <div className="grouping-header">Placeholder</div>
+                    <div className="settings-row">
+                        <div className="settings-name2">Placeholder</div>
+                        <div className="settings-value2">
+                            <span className="plus">test</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/myleague.component.js
+++ src/components/myleague.component.js
@@ -3,7 +3,191 @@ export default class MyLeague extends Component{
     render(){
         return(
             <div className="my-league-panel">
-                <h1>you are in my league</h1>
+                <div className="settings-section form-section">
+                    <div className="my-league-header">League Matchups</div>
+                   
+                    <div className="matchup-header" style={{marginTop:'20px'}}>
+                        <div className="matchup-row">
+                            <div className="matchup-owner">
+                                {/*left user information*/}
+                                <div className="left-matchup">
+                                    <div className="left-matchup-container">
+                                        <div className="name">Username</div>
+                                        <div className="team-name">Team 1</div>
+                                        <div className="description">0-0</div>
+                                    </div>
+                                </div>
+                                {/*left user score projection*/}
+                                <div className="score-and-projections">
+                                    <div className="score">0.00</div>
+                                    <div className="projection">0.00</div>
+                                </div>
+                            </div>
+
+                            {/*VS Label*/}
+                            <div className="vs-label">VS</div>
+                            
+                            {/*right user score projection*/}
+                            <div className="matchup-owner-flip">
+                                <div className="score-and-projections-flip">
+                                    <div className="score-flip">0.00</div>
+                                    <div className="projection-flip">0.00</div>
+                                </div>
+
+                                {/*right user information*/}
+                                <div className="right-matchup">
+                                    <div className="right-matchup-container">
+                                        <div className="name-flip">Username</div>
+                                        <div className="team-name-flip">Team 2</div>
+                                        <div className="description-flip">0-0</div>
+                                    </div>
+                                </div>
+                            </div>
+                                {/*user chance bar*/}
+                                <div className="middle-div">
+                                    <div className="above-chance">
+                                        <div className="left-chance">50%</div>
+                                        <div className="middle-chance">Win%</div>
+                                        <div className="right-chance">50%</div>
+                                    </div>
+                                    <div className="win-chance-bar">
+                                        <div className="left-bar"></div>
+                                        <div className="right-bar"></div>
+                                    </div>
+                                </div>
+                        </div>
+                        <div className="my-league-div"></div>
+
+                        <div className="matchup-header">
+                        <div className="matchup-row">
+                            <div className="matchup-owner">
+                                {/*left user information*/}
+                                <div className="left-matchup">
+                                    <div className="left-matchup-container">
+                                        <div className="name">Username</div>
+                                        <div className="team-name">Team 3</div>
+                                        <div className="description">0-0</div>
+                                    </div>
+                                </div>
+                                {/*left user score projection*/}
+                                <div className="score-and-projections">
+                                    <div className="score">0.00</div>
+                                    <div className="projection">0.00</div>
+                                </div>
+                            </div>
+
+                            {/*VS Label*/}
+                            <div className="vs-label">VS</div>
+                            
+                            {/*right user score projection*/}
+                            <div className="matchup-owner-flip">
+                                <div className="score-and-projections-flip">
+                                    <div className="score-flip">0.00</div>
+                                    <div className="projection-flip">0.00</div>
+                                </div>
+
+                                {/*right user information*/}
+                                <div className="right-matchup">
+                                    <div className="right-matchup-container">
+                                        <div className="name-flip">Username</div>
+                                        <div className="team-name-flip">Team 4</div>
+                                        <div className="description-flip">0-0</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                             {/*user chance bar*/}
+                                <div className="middle-div">
+                                    <div className="above-chance">
+                                        <div className="left-chance">50%</div>
+                                        <div className="middle-chance">Win%</div>
+                                        <div className="right-chance">50%</div>
+                                    </div>
+                                    <div className="win-chance-bar">
+                                        <div className="left-bar"></div>
+                                        <div className="right-bar"></div>
+                                    </div>
+                                </div>
+                                
+                        </div>
+                        <div className="my-league-div"></div>
+                    </div>
+                </div>
+                
+            </div>
+
+                <div className="settings-section form-section">
+                    <div className="my-league-header" >
+                        Standings
+                        <div className="my-league-waiver">Waiver</div>
+                        <div className="my-league-points-allowed">PA</div>
+                        <div className="my-league-points-for">PF</div>  
+                    </div>
+
+                    <div className="league-standing-row">
+                        <div className="rank">1</div>
+                            <div className="team-container">
+                                <div className="owner-av"></div>
+                                <div className="team-owner-name">Username 1</div>
+                                <div className="team-owner-desc">0-0</div>
+                            </div>
+                        <div className="points-for">0.0</div>
+                        <div className="points-for">0.0</div>
+                        <div className="waiver">1</div>
+                    </div>
+                    
+                    <div className="league-standing-row">
+                        <div className="rank">2</div>
+                        <div className="team-container">
+                            <div className="owner-av"></div>
+                            <div className="team-owner-name">Username 2</div>
+                            <div className="team-owner-desc">0-0</div>
+                        </div>
+                        <div className="points-for">0.0</div>
+                        <div className="points-for">0.0</div>
+                        <div className="waiver">2</div>
+                    </div>
+
+                    <div className="league-standing-row">
+                        <div className="rank">3</div>
+                        <div className="team-container">
+                            <div className="owner-av"></div>
+                            <div className="team-owner-name">Username 3</div>
+                            <div className="team-owner-desc">0-0</div>
+                        </div>
+                        <div className="points-for">0.0</div>
+                        <div className="points-for">0.0</div>
+                        <div className="waiver">3</div>
+                    </div>
+
+                    <div className="league-standing-row">
+                        <div className="rank">4</div>
+                        <div className="team-container">
+                            <div className="owner-av"></div>
+                            <div className="team-owner-name">Username 4</div>
+                            <div className="team-owner-desc">0-0</div>
+                        </div>
+                        <div className="points-for">0.0</div>
+                        <div className="points-for">0.0</div>
+                        <div className="waiver">4</div>
+                    </div>
+                </div>
+
+                <div className="settings-section form-section">
+                    <div className="my-league-header">Recent League Activity</div>
+                    <div className="league-transaction-item">
+                        <div className="team-owner-with-desc">
+                            <div className="av"></div>
+                            <div className="team-owner">Username</div>
+                            <div className="trans-timestamp">placeholder</div>
+                        </div>
+                        <div className="trans-action-label">-</div>
+                        <div className="trans-player-name">
+                            player name
+                            <div className="player-pos">player position</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
